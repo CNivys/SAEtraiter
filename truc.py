2f9840e1235d50f8b9e6bb18538f54b0a3618388
CNivys <112942508+CNivys@users.noreply.github.com>
2023-01-11 13:42:36 +0100

    Update truc.py

--- truc.py
+++ truc.py
@@ -32,9 +32,46 @@ for i in range(nb):
         tab.append(ti)
         print(ti, end=" ")
 
-tab.sort()
+def fusion(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def fusion_sort(tab):
+    if len(tab) < 2:
+        return tab
+    middle = len(tab) // 2
+    left = fusion_sort(tab[:middle])
+    right = fusion_sort(tab[middle:])
+    return fusion(left, right)
+
+def recherche_dichotomique(element, tab):
+    a = 0
+    b = len(tab)-1
+    m = (a+b)//2
+    while a < b :
+        if tab[m] == element:
+            return m
+        elif tab[m] > element:
+            b = m-1
+        else :
+            a = m+1
+        m = (a+b)//2
+    return a
+
+tab = fusion_sort(tab)
 print("\n")
 print(tab)
+print(recherche_dichotomique(2, tab))
 
 #histogramme:
 
@@ -44,3 +81,4 @@ plt.xticks(range(0,46))
 plt.ylabel('Nombres')
 plt.title("Histogrammes des tirages")
 plt.show()
+
